10e60607e2d3cb0f8ae052d816d69c1f814964b9
Subashini-G <subashinig2702@gmail.com>
2023-01-23 12:45:53 +0530

    login commit

---
+++ loginPage/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace loginPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+    void registerbutton_clicked(object sender, EventArgs eventArgs)
+
+    {
+        
+    }
+}
+
